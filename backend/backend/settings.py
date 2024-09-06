b480b81b67f5c914ef9aac45322ea45889c60584
LOUISE COOKE <louise.cooke@proton.me>
2024-09-06 08:31:50 -0700

    Temporarily remove corsheaders for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'corsheaders',
     'ourshelves',
-    'corsheaders',
     'rest_framework',
 ]
 
